89464d25d2cabff47e5c460cb207069f234579af
Oslo <80269655+osloviensole@users.noreply.github.com>
2025-08-24 17:17:19 +0100

    mise à jour dashboard

--- comptes/urls.py
+++ comptes/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
 
     # path('', views.HomeView.as_view(), name='home'),
     path("", views.CustomLoginView.as_view(), name="login"),
+
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
     path("choisir-ecole/", views.ChoisirEcoleView.as_view(), name="choisir_ecole"),
     path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
